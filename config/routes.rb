ed931829f71c3527a8e5fd18e15901e78312ab52
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-04-19 13:01:25 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
   resources :authors, only: %i[show index] do
     resources :posts, only: %i[show index new edit]
   end
 
   resources :posts, only: %i[index show new create edit update]
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 
   get '/admin/stats', to: 'stats#index'
 
